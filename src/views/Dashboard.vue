7296e40aa1e6b1719d06fc130a684cf221bb9719
jamestha3d <jamestha3d@gmail.com>
2024-10-19 22:22:50 -0400

    added docs link

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -128,12 +128,18 @@
 
       <!-- API Key -->
       <div class="mt-8">
-          <h2 class="text-lg leading-6 font-medium text-gray-900">API Key</h2>
+          <h2 class="text-lg leading-6 font-medium text-gray-900">Get an API Key</h2>
           
           <div class="mt-4 bg-white shadow overflow-hidden sm:rounded-md">
             
                   <div class="px-4 py-4 sm:px-6">
-                    <button @click="generateApiKey" class="w-full flex-shrink-0 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
+
+                    <div>
+                      
+                      <span> View the <a style="color:blue; text-decoration: underline" target="blank" href="https://easyprep-django.onrender.com/docs">docs</a> </span>
+                    </div>
+                    <br/> 
+                    <button @click="generateApiKey" class="w-200 flex-shrink-0 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
                             Generate API Key
                           </button>
                     <!-- <div class="flex items-center justify-between">
